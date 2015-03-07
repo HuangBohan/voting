b7703a0a387abe5f4de4d8def392c73037d904d8
tang <tangning111@gmail.com>
2015-03-07 12:31:08 +0800

    add constant host

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php 
+	require_once('includes/templates.php');
+	
+	if (session_status() == PHP_SESSION_NONE) {
+    	session_start();
+	}
+
+	if(isset($_GET['user'])) {
+		echo($_GET['user']);
+		echo($_SESSION['USER']);
+	}
+	else {
+		processLogin();
+		echo showLogin();
+	}
+?>
