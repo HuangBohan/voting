cc7d75a5746ad22d9545a82db7539eea92f5fafc
tang <tangning111@gmail.com>
2015-03-10 10:52:15 +0800

    clone a empty position template on load

--- includes/js/application.js
+++ includes/js/application.js
@@ -1,4 +1,6 @@
 $(function() {
+	var template = $(".position-template").first().clone();
+
 	$("#startDate").datetimepicker();
 	$("#endDate").datetimepicker();
 
@@ -7,23 +9,18 @@ $(function() {
 
 		var positionList = $(".position-template");
 		if(positionList.length < 4) {
-			var template = $(".position-template").first().clone();
-				
-			// clear all input fields	
-			template.find("input").val("");
-			var voterList = template.find("select");
-			voterList.val(voterList.find("option:first").val());
+			var tmp = template.clone();
 
 			// update all id fileds
 			var oldId 	= positionList.length - 1;
 			var newId 	= positionList.length;
-			template.find("#position-name-" + oldId).attr("id", "position-name-" + newId);
-			template.find("#position-slot-" + oldId).attr("id", "position-slot-" + newId);
-			template.find("#position-voter-" + oldId).attr("id", "position-voter-" + newId);
-			template.find("[for=position-name-" + oldId + "]").attr("for", "position-name-" + newId);
-			template.find("[for=position-slot-" + oldId + "]").attr("for", "position-slot-" + newId);
-			template.find("[for=position-voter-" + oldId + "]").attr("for", "position-voter-" + newId);
-			$(".position-template").last().after(template);	
+			tmp.find("#position-name-" + oldId).attr("id", "position-name-" + newId);
+			tmp.find("#position-slot-" + oldId).attr("id", "position-slot-" + newId);
+			tmp.find("#position-voter-" + oldId).attr("id", "position-voter-" + newId);
+			tmp.find("[for=position-name-" + oldId + "]").attr("for", "position-name-" + newId);
+			tmp.find("[for=position-slot-" + oldId + "]").attr("for", "position-slot-" + newId);
+			tmp.find("[for=position-voter-" + oldId + "]").attr("for", "position-voter-" + newId);
+			$(".position-template").last().after(tmp);	
 		}
 		
 	});
